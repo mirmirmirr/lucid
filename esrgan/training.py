bf4e819dc76dd596af24a06b6ff1fbeea1ed109e
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-06-16 17:22:59 -0400

    updated esrgan

--- esrgan/training.py
+++ esrgan/training.py
@@ -7,12 +7,14 @@ import opendatasets as od
 # using tensorflow funcational api
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
 import tensorflow_datasets as tfds
 
 import keras.layers as kl
-from tensorflow._api.v2.nn import depth_to_space
 import numpy as np
+
+from loss import VGG, Losses
+from data.preperation import justload
+from esrgan_arch import ESRGAN
 
 class Training(Model):
     def __init__(self, generator, discriminator, vgg, batchSize):
@@ -22,21 +24,12 @@ class Training(Model):
         self.vgg = vgg
         self.batchSize = batchSize
 
-        self.gen_opt = tf.keras.optimizers.Adam(3e-5)
-        self.disc_opt = tf.keras.optimizers.Adam(3e-5)
-        self.bceLoss = tf.losses.BinaryCrossentropy()
-        self.mseLoss = tf.losses.MeanSquaredError()
-
-    # def compile(self, gOptimizer, dOptimizer, bceLoss, mseLoss):
-    #     super().compile()
-	# 	# initialize the optimizers for the generator
-	# 	# and discriminator
-    #     self.gen_opt = gOptimizer
-    #     self.disc_opt = dOptimizer
-
-	# 	# initialize the loss functions
-    #     self.bceLoss = bceLoss
-    #     self.mseLoss = mseLoss
+    def compile(self, gOptimizer, dOptimizer, bceLoss, mseLoss):
+        super().compile()
+        self.gen_opt = gOptimizer
+        self.disc_opt = dOptimizer
+        self.bceLoss = bceLoss
+        self.mseLoss = mseLoss
 
     def train_step(self, images):
         (loRes, hiRes) = images
@@ -45,83 +38,65 @@ class Training(Model):
 
         superImages = self.gen(loRes)
         combined = tf.concat([superImages, hiRes], axis=0)
-
         labels = tf.concat([tf.zeros((self.batchSize, 1)), tf.ones((self.batchSize, 1))], axis=0)
 
         ## DISCRIMINATOR TRAINING
         with tf.GradientTape() as tape:
             rawPred = self.disc(combined)
-            rawFake = rawPred[:self.batchSize]
-            rawReal = rawPred[self.batchSize:]
+            rawFake, rawReal = tf.split(rawPred, num_or_size_splits=2, axis=0)
 
-            predFake = tf.keras.activations.sigmoid(rawFake - tf.math.reduce_mean(rawReal))
-            predReal = tf.keras.activations.sigmoid(rawReal - tf.math.reduce_mean(rawFake))
+            predFake = tf.keras.activations.sigmoid(rawFake - tf.reduce_mean(rawReal))
+            predReal = tf.keras.activations.sigmoid(rawReal - tf.reduce_mean(rawFake))
 
             predictions = tf.concat([predFake, predReal], axis=0)
-            disc_loss = self.bceLoss(labels, predictions)
+            dLoss = self.bceLoss(labels, predictions)
 
-        # calculate gradients
-        grad = tape.gradient(disc_loss, self.disc.trainable_variables)
-
-        # calculate weights and apply to model
+        grad = tape.gradient(dLoss, self.disc.trainable_variables)
         self.disc_opt.apply_gradients(zip(grad, self.disc.trainable_variables))
 
-        # generate misleading labels for generator training
         misleaders = tf.ones((self.batchSize, 1))
 
         ## GENERATOR TRAINING
         with tf.GradientTape() as tape:
             imposterImages = self.gen(loRes)
-
             rawPred = self.disc(imposterImages)
             realPred = self.disc(hiRes)
-            predictions = tf.keras.activations.sigmoid(rawPred - tf.math.reduce_mean(realPred))
+            predictions = tf.keras.activations.sigmoid(rawPred - tf.reduce_mean(realPred))
 
             gen_loss = self.bceLoss(misleaders, predictions)
             pixel_loss = self.mseLoss(hiRes, imposterImages)
 
             superVGG = tf.keras.applications.vgg19.preprocess_input(imposterImages)
-            superVGG = self.vgg(superVGG) / 12.75
-            hiresVGG = tf.keras.applications.vgg19.preprocess_input(hiRes)
-            hiresVGG = self.vgg(hiresVGG) / 12.75
+            superVGG = self.vgg(superVGG)
 
-            perceptualLoss = self.mseLoss(hiresVGG, superVGG)
-            totalLoss = 5e-3 * gen_loss + perceptualLoss + 1e-2 * pixel_loss
+            realVGG = tf.keras.applications.vgg19.preprocess_input(hiRes)
+            realVGG = self.vgg(realVGG)
 
-        # calc gradients
-        grad = tape.gradient(totalLoss, self.gen.trainable_variables)
+            perc_loss = self.mseLoss(realVGG, superVGG)
+            gLoss = pixel_loss + 0.001 * gen_loss + 0.006 * perc_loss
 
-        # optimize weights
+        grad = tape.gradient(gLoss, self.gen.trainable_variables)
         self.gen_opt.apply_gradients(zip(grad, self.gen.trainable_variables))
 
-        return {"disc": disc_loss, 'gen_totalLoss': totalLoss, "gen_loss": gen_loss, "perceptual_loss": perceptualLoss, "pixel_loss": pixel_loss}
-
-# def train(data, pretrainEPOCHS, finetuneEPOCHS):
-#     # loss = Losses()
-
-#     gen.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss=mseLoss)
-
-#     print("pretraining ESRGAN generator...")
-#     gen.fit(data, epochs=pretrainEPOCHS, steps_per_epoch=10, batch_size=64)
-
-#     if not os.path.exists('outputs'):
-#       os.makedirs('outputs')
-
-#     print('saving generator...')
-#     gen.save(PRETRAINED_GEN_MODEL)
-
-#     vgg = VGG.build()
-#     esrgan = Training(gen, disc, vgg, 64)
-
-#     print("training ESRGAN...")
-#     esrgan.fit(data, epochs=finetuneEPOCHS, steps_per_epoch=10)
-
-#     print('saving ESRGAN...')
-#     esrgan.gen.save(GENERATOR_MODEL)
-
-#     print('SAVED ALL MODELS ARE SAVE FINALLY AHHHHHHHHHHHHHHHHHHHHHHH')
+        return {"dLoss": dLoss, "gLoss": gLoss}
 
 if __name__ == "__main__":
+
+    ## initalizing opimizers and loss functions
+    gen_opt_pt = tf.keras.optimizers.legacy.Adam(1e-4)
+    disc_opt_pt = tf.keras.optimizers.legacy.Adam(1e-4)
+
+    gen_opt = tf.keras.optimizers.legacy.Adam(3e-5)
+    disc_opt = tf.keras.optimizers.legacy.Adam(3e-5)
+
+    bceLoss = tf.losses.BinaryCrossentropy()
+    mseLoss = tf.losses.MeanSquaredError()
+
+    PRETRAINED_GEN_MODEL = os.path.join('outputs', 'models', 'pretrained_generator')
+    GENERATOR_MODEL = os.path.join('outputs', 'models', 'generator')
+    pretrainEPOCHS = 1500
+    finetuneEPOCHS = 1000
+
     DATASET = "div2k/bicubic_x4"
 
     DIV2K_PATH = os.path.join('dataset', "div2k")
@@ -131,9 +106,17 @@ if __name__ == "__main__":
     # path to our base output directory
     BASE_OUTPUT_PATH = "outputs"
 
+    ## define multi-gpu strategy
+    strategy = tf.distribute.MirroredStrategy()
+
     # set the train TFRecords, pretrained generator, and final
     # generator model paths to be used for GPU training
     tfrTrainPath = GPU_TRAIN_PATH
+    pretrainedGenPath = PRETRAINED_GEN_MODEL
+    genPath = GENERATOR_MODEL
+
+    # set the batch size
+    BATCH_SIZE = 64
 
     # grab train TFRecord filenames
     print("[INFO] grabbing the train TFRecords...")
@@ -141,32 +124,46 @@ if __name__ == "__main__":
 
     # build the div2k datasets from the TFRecords
     print("[INFO] creating train and test dataset...")
-    trainDs = justload(filenames=trainTfr, train=True,batchSize=64)
+    trainDs = justload(filenames=trainTfr, train=True, batchSize=BATCH_SIZE * strategy.num_replicas_in_sync)
+    # trainDs = load_dataset(filenames=trainTfr, train=True,
+    # 	batchSize=64)
 
-    PRETRAINED_GEN_MODEL = os.path.join('outputs', 'models', 'pretrained_generator')
-    GENERATOR_MODEL = os.path.join('outputs', 'models', 'generator')
-    pretrainEPOCHS = 1500
-    finetuneEPOCHS = 1000
+    ESRGAN = ESRGAN()
+    gen = ESRGAN.generator()
+    disc = ESRGAN.discriminator(4, 0.2, 4)
 
     # train(trainDs, 1500, 1000)
-    gen.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss=mseLoss)
+    with strategy.scope():
+        losses = Losses(numReplicas = strategy.num_replicas_in_sync)
+        ESRGAN = ESRGAN()
+        gen = ESRGAN.generator()
+        gen.compile(optimizer=tf.keras.optimizers.Adam(1e-4), loss=losses.mse_loss)
 
-    print("pretraining ESRGAN generator...")
-    gen.fit(trainDs, epochs=pretrainEPOCHS, steps_per_epoch=10, batch_size=64)
+        print("pretraining ESRGAN generator...")
+        # gen.fit(trainDs, epochs=pretrainEPOCHS, steps_per_epoch=10, batch_size=64)
+        gen.fit(trainDs, epochs=pretrainEPOCHS, steps_per_epoch=10)
 
     if not os.path.exists('outputs'):
         os.makedirs('outputs')
 
     print('saving generator...')
-    gen.save(PRETRAINED_GEN_MODEL)
+    gen.save(pretrainedGenPath)
 
-    vgg = VGG.build()
-    esrgan = Training(gen, disc, vgg, 64)
+    with strategy.scope():
+        losses = Losses(numReplicas = strategy.num_replicas_in_sync)
 
-    print("training ESRGAN...")
-    esrgan.fit(trainDs, epochs=finetuneEPOCHS, steps_per_epoch=10)
+        ESRGAN = ESRGAN()
+        gen = ESRGAN.generator()
+        disc = ESRGAN.discriminator(4, 0.2, 4)
+
+        vgg = VGG.build()
+        esrgan = Training(gen, disc, vgg, batchSize=64)
+        esrgan.compile(tf.keras.optimizers.Adam(3e-5), tf.keras.optimizers.Adam(3e-5), losses.bce_loss, losses.mse_loss)
+
+        print("training ESRGAN...")
+        esrgan.fit(trainDs, epochs=finetuneEPOCHS, steps_per_epoch=10)
 
     print('saving ESRGAN...')
-    esrgan.gen.save(GENERATOR_MODEL)
+    esrgan.gen.save(genPath)
 
     print('SAVED ALL MODELS ARE SAVE FINALLY AHHHHHHHHHHHHHHHHHHHHHHH')
